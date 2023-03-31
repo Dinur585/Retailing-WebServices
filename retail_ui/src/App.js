cafc37e3690410372c9b01c330a183a4438cb906
Thymmz <atharva.deshpande228@gmail.com>
2023-03-31 12:58:54 -0500

    Added orders page

--- retail_ui/src/App.js
+++ retail_ui/src/App.js
@@ -1,23 +1,24 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Auth from "./components/Auth";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Items from "./components/Items";
-import { useContext } from 'react';
-import { Store } from './Store';
+import { useContext } from "react";
+import { Store } from "./Store";
 import ProfileScreen from "./components/ProfileScreen";
+import Orders from "./components/Orders";
 function App() {
   const { state } = useContext(Store);
   const { cart } = state;
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<Auth />}/>
-        <Route path="/items" element={<Items />}/>
-        <Route path="/PROFILE" element={<ProfileScreen />}/>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/PROFILE" element={<ProfileScreen />} />
       </Routes>
     </Router>
-    
   );
 }
 
--- retail_ui/src/components/Items.js
+++ retail_ui/src/components/Items.js
@@ -1,94 +1,90 @@
-import pic from "../images/bookImg.jpg";
-import { Store } from '../Store';
-import { useContext, useState, useEffect } from 'react';
-import '../index.css';
-import '../styles/Items.css'
-import { DataTable } from 'primereact/datatable';
-import { InputSwitch } from 'primereact/inputswitch';
-import { Column } from 'primereact/column';
+import { useState, useEffect } from "react";
+import "../index.css";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import axios from "axios";
-import { Button } from "react-bootstrap";
 
 //const mockData = require('./items.json');
 
-
 function Items() {
-    //const [products, setProducts] = useState([]);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const [metaKey, setMetaKey] = useState(true);
+  //   const [products, setProducts] = useState([]);
+  //   useEffect(() => {
+  //     axios.get("http://localhost:8010/api/item/items").then((res) => {
+  //       // console.log(res.data);
+  //       setProducts(res.data);
+  //       console.log(products);
+  //     });
+  //   }, []);
 
-    // useEffect(() => {
-    //     axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
-    //         // console.log(res.data);
-    //         setProducts(res.data);
-    //         console.log(products);
-    //     });
-    // }, []);
+  const products = [
+    {
+      product_id: 4,
+      product: "Loaded Cup Dispenser",
+      price: 90.08,
+      quantity: 14,
+      image: "/images/bookImg.jpg",
+    },
+    {
+      product_id: 4,
+      product: "Juice - Orange, 341 Ml",
+      price: 90.08,
+      quantity: 14,
+    },
+    {
+      product_id: 4,
+      product: "Cake - Bande Of Fruit",
+      price: 90.08,
+      quantity: 14,
+    },
+    {
+      product_id: 4,
+      product: "Ice Cream - Chocolate",
+      price: 90.08,
+      quantity: 14,
+    },
+    {
+      product_id: 4,
+      product: "Cleaner - Pine Sol",
+      price: 90.08,
+      quantity: 14,
+    },
+    {
+      product_id: 4,
+      product: "Seaweed Green Sheets",
+      price: 90.08,
+      quantity: 14,
+    },
+  ];
 
-    const products=[
-        {
-          "product_id": 4,
-          "product": "Loaded Cup Dispenser",
-          "price": 90.08,
-          "quantity": 14,
-          "image":'/images/bookImg.jpg'
-        },
-        {
-            "product_id": 4,
-            "product": "Juice - Orange, 341 Ml",
-            "price": 90.08,
-            "quantity": 14
-        },
-        {
-            "product_id": 4,
-            "product": "Cake - Bande Of Fruit",
-            "price": 90.08,
-            "quantity": 14
-        },
-        {
-            "product_id": 4,
-            "product": "Ice Cream - Chocolate",
-            "price": 90.08,
-            "quantity": 14
-        },
-        {
-            "product_id": 4,
-            "product": "Cleaner - Pine Sol",
-            "price": 90.08,
-            "quantity": 14
-        },
-        {
-            "product_id": 4,
-            "product": "Seaweed Green Sheets",
-            "price": 90.08,
-            "quantity": 14
-        }]
-
+  const renderButton = (rowData) => {
     return (
-        <div className="productTableContainer">
-            <table className="productTable">
-                <thead>
-                    <tr>
-                        <th>Product Id</th>
-                        <th>Product Name</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th> </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map((product)=>{
-                        return (<tr>
-                            <td>{product.product_id}</td>
-                            <td>{product.product}</td>
-                            <td>{product.price}</td>
-                            <td>{product.quantity}</td>
-                            <td><button className="btn btn-info">Add</button></td>
-                        </tr>)
-                    })}
-                </tbody>
-            </table>
-        </div>
+      <button
+        onClick={() => {
+          console.log(rowData.id);
+        }}
+      >
+        Add to Cart{" "}
+      </button>
     );
+  };
+
+  return (
+    <div>
+      <h2>Item Details</h2>
+      <div>
+        <main>
+          <>
+            <DataTable value={products} tableStyle={{ minWidth: "50rem" }}>
+              <Column field="product_id" header="ProductId"></Column>
+              <Column field="product" header="Product Name"></Column>
+              <Column field="price" header="Price"></Column>
+              <Column field="quantity" header="Quantity"></Column>
+              <Column body={renderButton}></Column>
+            </DataTable>
+          </>
+        </main>
+      </div>
+    </div>
+  );
 }
 export default Items;
