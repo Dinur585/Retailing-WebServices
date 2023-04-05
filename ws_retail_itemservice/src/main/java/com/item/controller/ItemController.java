7d913a53b8c7b7adda42906e261976ffe77165b3
Thymmz <atharva.deshpande228@gmail.com>
2023-04-05 10:26:28 -0500

    Removed CORS properties from itemservice

--- ws_retail_itemservice/src/main/java/com/item/controller/ItemController.java
+++ ws_retail_itemservice/src/main/java/com/item/controller/ItemController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
