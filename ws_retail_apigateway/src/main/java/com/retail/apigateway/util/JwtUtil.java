a919c0bd43cdb2998fed23bb60d19e169b4e8d69
Thymmz <atharva.deshpande228@gmail.com>
2023-04-10 11:32:28 -0500

    JWT validation logic

---
+++ ws_retail_apigateway/src/main/java/com/retail/apigateway/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.retail.apigateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtUtil {
+    public static final String SECRET = "566D597133743677397A244326452948404D635166546A576E5A723475377821";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
